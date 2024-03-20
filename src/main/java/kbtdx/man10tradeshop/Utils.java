2b991f1c226d7ae1fb0be2e2c93ec7ee712fcc09
kabutokunn <danieru.hakka@gmail.com>
2024-03-20 16:50:50 +0900

    まあまあすすんだ

--- src/main/java/kbtdx/man10tradeshop/Utils.java
+++ src/main/java/kbtdx/man10tradeshop/Utils.java
@@ -4,6 +4,13 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.*;
 
 public class Utils {
@@ -25,5 +32,40 @@ public class Utils {
         meta.setLore(lore);
         item.setItemMeta(meta);
         return item;
+    }
+
+    public String itemToBase64(ItemStack item) throws IllegalStateException {
+        if (item == null || item.getType() == Material.AIR) {
+            return null;
+        }
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+            ItemStack[] items = new ItemStack[1];
+            items[0] = item;
+            dataOutput.writeInt(items.length);
+            for (int i = 0; i < items.length; i++) {
+                dataOutput.writeObject(items[i]);
+            }
+            dataOutput.close();
+            return Base64.getEncoder().encodeToString(outputStream.toByteArray());
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to save item stacks.", e);
+        }
+    }
+
+    public ItemStack itemFromBase64(String data) {
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            ItemStack[] items = new ItemStack[dataInput.readInt()];
+            for (int i = 0; i < items.length; i++) {
+                items[i] = (ItemStack) dataInput.readObject();
+            }
+            dataInput.close();
+            return items[0];
+        } catch (IOException | ClassNotFoundException e) {
+            return null;
+        }
     }
 }
